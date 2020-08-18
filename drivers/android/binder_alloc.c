53a3efb64f2a67a84f7315609da8099282b4cc2e
YangHui <yanghui.def@gmail.com>
2020-08-18 09:34:04 +0800

    binder: Modify comments
    The function name should is binder_alloc_new_buf()
    
    Signed-off-by: YangHui <yanghui.def@gmail.com>
    Reviewed-by: Martijn Coenen <maco@android.com>
    Link: https://lore.kernel.org/r/1597714444-3614-1-git-send-email-yanghui.def@gmail.com
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/android/binder_alloc.c
+++ drivers/android/binder_alloc.c
@@ -748,7 +748,7 @@ static void binder_alloc_clear_buf(struct binder_alloc *alloc,
  * @alloc:	binder_alloc for this proc
  * @buffer:	kernel pointer to buffer
  *
- * Free the buffer allocated via binder_alloc_new_buffer()
+ * Free the buffer allocated via binder_alloc_new_buf()
  */
 void binder_alloc_free_buf(struct binder_alloc *alloc,
 			    struct binder_buffer *buffer)
