5b9cbb82573254cb9e209a7d9a10baf50cf824f5
tanz3 <3222839916@qq.com>
2022-05-05 09:54:28 +0000

    techpack: audio: Import xiaomi changes * From odin-r-oss
    Signed-off-by: tanz3 <3222839916@qq.com>

--- techpack/audio/dsp/q6voice.c
+++ techpack/audio/dsp/q6voice.c
@@ -2,7 +2,9 @@
 /*
  * Copyright (c) 2012-2021, The Linux Foundation. All rights reserved.
  * Copyright (c) 2022 Qualcomm Innovation Center, Inc. All rights reserved.
+ * Copyright (C) 2021 XiaoMi, Inc.
  */
+#define DEBUG
 #include <linux/slab.h>
 #include <linux/kthread.h>
 #include <linux/kernel.h>
